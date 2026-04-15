7d16a2c2225e538a8e346b7a36eec5f313b6e6c0
emclavel <emilie.clavel@bellmedia.ca>
2026-04-15 11:12:15 -0400

    Fix CSV columns mapping to match GeoJSON schema

--- fetch_regie_essence.py
+++ fetch_regie_essence.py
@@ -1,7 +1,6 @@
 import os
 import requests
 import csv
-import json
 from datetime import datetime
 
 URL = "https://regieessencequebec.ca/stations.geojson.gz"
@@ -17,41 +16,43 @@ headers = {
 response = requests.get(URL, headers=headers, timeout=30)
 response.raise_for_status()
 
-# requests a déjà décompressé si nécessaire
+# requests gère automatiquement la décompression gzip
 data = response.json()
 
 with open(CSV_PATH, "w", newline="", encoding="utf-8") as csvfile:
     writer = csv.writer(csvfile)
 
+    # EN-TÊTES ALIGNÉES AVEC LE SCHÉMA RÉEL
     writer.writerow([
         "nom_station",
         "banniere",
         "adresse",
-        "ville",
-        "region",
+        "municipalite",
+        "mrc",
+        "code_postal",
         "prix_regulier",
         "prix_super",
         "prix_diesel",
         "longitude",
         "latitude",
-        "updated_at",
+        "date_mise_a_jour",
         "date_import"
     ])
 
     for feature in data["features"]:
         props = feature.get("properties", {})
         coords = feature["geometry"]["coordinates"]
-        prices = props.get("prices", {})
 
         writer.writerow([
             props.get("name"),
-            props.get("brand"),
+            props.get("banner"),
             props.get("address"),
-            props.get("city"),
-            props.get("region"),
-            prices.get("regular"),
-            prices.get("super"),
-            prices.get("diesel"),
+            props.get("municipality"),
+            props.get("mrc"),
+            props.get("postal_code"),
+            props.get("price_regular"),
+            props.get("price_super"),
+            props.get("price_diesel"),
             coords[0],
             coords[1],
             props.get("updated_at"),
